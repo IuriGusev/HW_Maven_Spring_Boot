324f01b6bb693f1b70f9b8de821247b9c97dfad4
iurii_gusev <gusevyuv@gmail.com>
2023-08-17 14:19:35 -0400

    first commit

---
+++ src/main/java/pro/sky/HWMavenSpring/Boot/HwMavenSpringBootApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.HWMavenSpring.Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwMavenSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwMavenSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/HWMavenSpring/Boot/HwMavenSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.HWMavenSpring.Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwMavenSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
